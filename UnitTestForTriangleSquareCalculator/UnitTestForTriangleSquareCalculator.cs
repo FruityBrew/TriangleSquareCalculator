c7b25da48c97aaa57a2877affe62f39aec015613
FruityBrew <kompota@yandex.ru>
2016-03-08 21:13:40 +0300

    Test

---
+++ UnitTestForTriangleSquareCalculator/UnitTestForTriangleSquareCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XCompanyLib;
+
+namespace UnitTestForTriangleSquareCalculator
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BadArguments()
+        {
+            XTriangleSquareCalculator.TriangleSqureCalculate(-3, 4, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NotRightTriangle()
+        {
+            XTriangleSquareCalculator.TriangleSqureCalculate(12, 11, 13);
+        }
+
+        [TestMethod]
+        public void RightTriangleArgSequence_1()
+        {
+            Assert.AreEqual(XTriangleSquareCalculator.TriangleSqureCalculate(7, 24, 25), 84);
+        }
+
+        [TestMethod]
+        public void RightTriangleArgSequence_2()
+        {
+            Assert.AreEqual(XTriangleSquareCalculator.TriangleSqureCalculate(5, 4, 3), 6);
+        }
+
+        [TestMethod]
+        public void RightTriangleArgSequence_3()
+        {
+            Assert.AreEqual(XTriangleSquareCalculator.TriangleSqureCalculate(12, 13, 5), 30);
+        }
+
+    }
+}
---
+++ XCompanyLib/XTriangleSquareCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace XCompanyLib
+{
+    public static class XTriangleSquareCalculator
+    {
+        public static Double TriangleSqureCalculate(Double sideA, Double sideB, Double sideC) 
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0) 
+                throw new ArgumentOutOfRangeException("Values can not be less than or equal zero");
+
+            var hypotenuse = sideA;
+            var cathetusA = sideB;
+            var cathetusB = sideC;
+
+            if (sideB > hypotenuse)
+                {
+                    hypotenuse = sideB;
+                    cathetusA = sideA;
+                }
+
+            if (sideC > hypotenuse)
+                {
+                    hypotenuse = sideC;
+                    cathetusB = sideB;
+                    cathetusA = sideA;  
+                }            
+
+            if ( hypotenuse * hypotenuse != cathetusA * cathetusA + cathetusB * cathetusB)
+                throw new ArgumentException("A triangle which sides were inputted is not a right triangle");
+            
+            return cathetusA * cathetusB / 2;                    
+        }
+
+    }
+}
